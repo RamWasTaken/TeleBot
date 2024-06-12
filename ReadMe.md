39e915184572b3f74f742fc0bcbf8876f2b3fbbd
RamWasTaken <ramkardile4ever@gmail.com>
2024-06-12 08:35:57 +0530

    cloned stuff

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+The Spline had Issuse with React 
+TypeError: Cannot read properties of undefined (reading 'A')
+no one is able to solve this issue
+```
+no response from spline team
+```
---
+++ TO-DO app/src/App.jsx
@@ -0,0 +1,17 @@
+import NavbarComponent from './Components/Navbar'
+import Card from './Components/Card'
+import Footer from './Components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <div  className="App">
+      <NavbarComponent></NavbarComponent>
+      <Card></Card>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
